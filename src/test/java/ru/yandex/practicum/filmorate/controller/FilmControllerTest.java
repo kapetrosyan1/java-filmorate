e902d650e97ef160bd218fb2f907c70ba0082499
kapetrosyan1 <127433632+kapetrosyan1@users.noreply.github.com>
2024-01-16 17:27:52 +0300

    Update FilmControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
-import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,12 +26,12 @@ public class FilmControllerTest {
     @Test
     void createFilmTest() {
         Film film1 = new Film(1, "TestFilm1", "TestFilm1Description",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null);
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null);
 
         assertThrows(AlreadyExistException.class, () -> filmController.createFilm(film1), "Должен выбросить исключение");
 
         Film film = filmController.createFilm(new Film(0, "TestFilm", "TestFilmDescription",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null));
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null));
 
 
         assertEquals(1, filmController.findAll().size());
@@ -42,11 +41,11 @@ public class FilmControllerTest {
     @Test
     void filmValidationTest() {
         Film film2 = new Film(0, null, "TestFilm2Description",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null);
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null);
         Film film3 = new Film(0, "TestFilm3", "TestFilm3Description",
-                LocalDate.of(1815, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null);
+                LocalDate.of(1815, 11, 3), 120, null, new HashSet<>(), null);
         Film film4 = new Film(0, "TestFilm4", "TestFilm4Description",
-                LocalDate.of(1996, 11, 3), 0, new TreeSet<>(), new HashSet<>(), null);
+                LocalDate.of(1996, 11, 3), 0, null, new HashSet<>(), null);
 
         assertThrows(ValidationException.class, () -> filmController.createFilm(film2), "Должен выбросить исключение");
         assertThrows(ValidationException.class, () -> filmController.createFilm(film3), "Должен выбросить исключение");
@@ -56,14 +55,14 @@ public class FilmControllerTest {
     @Test
     void updateFilmTest() {
         filmController.createFilm(new Film(0, "TestFilm", "TestFilmDescription",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null));
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null));
         Film nonUpdatable = new Film(4, "Non Updatable", "TestFilmDescription",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null);
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null);
 
         assertThrows(DoesNotExistException.class, () -> filmController.updateFilm(nonUpdatable), "Должен выбросить исключение");
 
         Film updatedFilm = filmController.updateFilm(new Film(1, "TestFilm", "TestFilmDescription",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null));
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null));
 
         assertEquals(1, filmController.findAll().size(), "Должен быть только 1 фильм");
         assertEquals(updatedFilm, filmController.findAll().get(0));
@@ -76,7 +75,7 @@ public class FilmControllerTest {
         assertTrue(emptyFilmList.isEmpty(), "Фильмы пока не были добавлены");
 
         Film film1 = filmController.createFilm(new Film(0, "TestFilm", "TestFilmDescription",
-                LocalDate.of(1996, 11, 3), 120, new TreeSet<>(), new HashSet<>(), null));
+                LocalDate.of(1996, 11, 3), 120, null, new HashSet<>(), null));
         assertEquals(1, filmController.findAll().size(), "Неверный размер списка фильмов");
         assertEquals(film1, filmController.findAll().get(0), "Сохранен не тот фильм");
     }
